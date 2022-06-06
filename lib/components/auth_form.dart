0138e78e73f00e5dbb7c1263f8687d83c376e4e0
Wellyson Pontes <wellysonpontes@gmail.com>
2022-06-06 12:55:45 -0300

    feito configuração de login

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,10 +1,31 @@
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class AuthForm extends StatelessWidget {
-  const AuthForm({Key? key}) : super(key: key);
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    Key? key,
+    required this.onSubmit,
+  }) : super(key: key);
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    widget.onSubmit(_formData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +34,70 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
+              if (_formData.isSingUp)
+                TextFormField(
+                  key: ValueKey("name"),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: InputDecoration(labelText: "Nome"),
+                  validator: (_name) {
+                    final name = _name ?? "";
+                    if (name.trim().length < 5) {
+                      return "Novo deve conter no mínimo 5 caracteres";
+                    }
+                    return null;
+                  },
+                ),
               TextFormField(
-                decoration: InputDecoration(labelText: "Nome"),
-              ),
-              TextFormField(
+                key: ValueKey("email"),
+                initialValue: _formData.email,
+                onChanged: (email) => _formData.email = email,
                 decoration: InputDecoration(labelText: "E-mail"),
+                validator: (_email) {
+                  final email = _email ?? "";
+                  if (!email.contains("@")) {
+                    return "E-mail inválido";
+                  }
+                  return null;
+                },
               ),
               TextFormField(
+                key: ValueKey("passord"),
+                initialValue: _formData.password,
+                onChanged: (password) => _formData.password = password,
                 decoration: InputDecoration(labelText: "Senha"),
                 obscureText: true,
+                validator: (_password) {
+                  final password = _password ?? "";
+                  if (password.length < 6) {
+                    return "Senha deve conter no mínimo 6 caracteres";
+                  }
+                  return null;
+                },
               ),
+              if (_formData.isSingUp)
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Confirmar Senha"),
+                  obscureText: true,
+                ),
               SizedBox(height: 12),
               ElevatedButton(
-                onPressed: () {},
-                child: Text("Entrar"),
+                child: Text(_formData.isLogin ? "Entrar" : "Cadastrar"),
+                onPressed: _submit,
               ),
-              TextButton(onPressed: () {}, child: Text("Criar uma nova conta"))
+              TextButton(
+                child: Text(_formData.isLogin
+                    ? "Criar uma nova conta"
+                    : "Entrar com conta existente"),
+                onPressed: () {
+                  setState(() {
+                    _formData.toggleAuthMode();
+                  });
+                },
+              )
             ],
           ),
         ),
--- lib/models/auth_form_data.dart
+++ lib/models/auth_form_data.dart
@@ -5,15 +5,19 @@ enum AuthMode { SignUp, Login }
 class AuthFormData {
   String name = "";
   String email = "";
-  String passord = "";
+  String password = "";
   File? image;
   AuthMode _mode = AuthMode.Login;
 
-  bool get isLoging {
+  bool get isLogin {
     return _mode == AuthMode.Login;
   }
 
   bool get isSingUp {
     return _mode == AuthMode.SignUp;
+  }
+
+  void toggleAuthMode() {
+    _mode = isLogin ? AuthMode.SignUp : AuthMode.Login;
   }
 }
